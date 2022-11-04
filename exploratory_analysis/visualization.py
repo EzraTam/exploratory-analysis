3dd445883d411ad6d7fdb192b0507292a3fb99c7
Ezra Tampubolon <ezra.tampubolon@googlemail.com>
2022-11-04 20:49:39 +0100

    Refactor

--- exploratory_analysis/visualization.py
+++ exploratory_analysis/visualization.py
@@ -2,10 +2,48 @@
 """
 
 from collections import Counter
-from typing import List, Tuple, Optional
+from typing import Any, List, Tuple, Optional
+from matplotlib.axes import Axes
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+from exploratory_analysis import basic_functions as bf
+
+
+def boxplot_list(
+    data: pd.DataFrame, x: Any, list_y: List[Any], list_axes=List[Axes]
+) -> None:
+    for y in list_y:
+        sns.boxplot(
+            ax=list_axes[list_y.index(y)],
+            data=data,
+            x=x,
+            y=y,
+            showfliers=False,
+        )
+
+
+def piechart_list(
+    li_data: List[pd.Series], labels: List[str], list_axes: List[Axes]
+) -> None:
+
+    colors = sns.color_palette("pastel")[0:5]
+
+    for ax, data in zip(list_axes, li_data):
+        ax.pie(
+            x=data,
+            labels=labels,
+            colors=colors,
+            explode=bf.set_zero_if_below(data / sum(data), 0.1),
+            autopct="%.0f%%",
+            pctdistance=1.1,
+            labeldistance=1.2,
+            shadow=True,
+            radius=1,
+            rotatelabels=True,
+            textprops={"fontsize": 8},
+        )
 
 
 def plot_boxplots(
@@ -30,20 +68,18 @@ def plot_boxplots(
     assert (
         x_feature in data.columns
     ), "Data does not have the queried column for the x-axis"
-    for y_feature in li_y_features:
-        assert (
-            y_feature in data.columns
-        ), "Data does not have the queried column for the x-axis"
+
+    assert len(li_y_features) <= len(
+        bf.distinct(li_y_features)
+    ), "Elements in li_y_features has to be distinct"
+
+    assert set(li_y_features).issubset(
+        data.columns
+    ), "Data does not have the queried column for the x-axis"
+
     fig, axes = plt.subplots(1, len(li_y_features), figsize=figsize)
     fig.suptitle(title)
-    for y_feature in li_y_features:
-        sns.boxplot(
-            ax=axes[li_y_features.index(y_feature)],
-            data=data,
-            x=x_feature,
-            y=y_feature,
-            showfliers=False,
-        )
+    boxplot_list(data=data, x=x_feature, list_y=li_y_features, list_axes=axes)
 
 
 def create_pie_chart_from_multi_idx_df(
@@ -63,54 +99,32 @@ def create_pie_chart_from_multi_idx_df(
             desirely. Optional!
     """
 
-    if data.index.nlevels != 2:
-        raise ValueError("DF has to have 2 level indexes")
-
-    fg_order_subcategory = False
-    if ordering_sub_category is None:
-        fg_order_subcategory = True
-        if Counter(ordering_sub_category) != Counter(
-            list(set(data.index.get_level_values(1)))
-        ):
-            raise ValueError(
-                "ordering_sub_category has to be a permutation of the second level indexes"
-            )
+    bf.raise_error(
+        (data.index.nlevels != 2), ValueError, "DF has to have 2 level indexes"
+    )
 
     categories = list(set(data.index.get_level_values(0)))
+    sub_categories = list(set(data.index.get_level_values(1)))
+
+    bf.raise_error(
+        condition=(ordering_sub_category is not None)
+        and (Counter(ordering_sub_category) != Counter(sub_categories)),
+        error_class=ValueError,
+        msg="ordering_sub_category has to be a permutation of the second level indexes",
+    )
+
     fig, axes = plt.subplots(1, len(categories), figsize=(25, 8))
     fig.suptitle(title)
-    for category in categories:
-        data_raw = data.xs(category)[col_plotted]
 
-        if fg_order_subcategory:
-            data_plot = [
-                data_raw[sub_category] for sub_category in ordering_sub_category
-            ]
-            labels = ordering_sub_category
-        else:
-            data_plot = data_raw
-            labels = list(set(data.index.get_level_values(1)))
+    li_data_raw = [data.xs(category)[col_plotted] for category in categories]
 
-        explode = [val if val >= 0.1 else 0 for val in data_plot / sum(data_plot)]
+    data_processed = [
+        bf.return_obj_val_in_list_order(data_raw, ordering_sub_category)
+        if ordering_sub_category
+        else data_raw
+        for data_raw in li_data_raw
+    ]
 
-        # define Seaborn color palette to use
-        colors = sns.color_palette("pastel")[0:5]
+    labels = ordering_sub_category if ordering_sub_category else sub_categories
 
-        ax_idx = categories.index(category)
-
-        axes[ax_idx].set_title(category, pad=40)
-
-        # create pie chart
-        axes[ax_idx].pie(
-            x=data_plot,
-            labels=labels,
-            colors=colors,
-            explode=explode,
-            autopct="%.0f%%",
-            pctdistance=1.1,
-            labeldistance=1.2,
-            shadow=True,
-            radius=1,
-            rotatelabels=True,
-            textprops={"fontsize": 8},
-        )
+    piechart_list(li_data=data_processed, labels=labels, list_axes=axes)
