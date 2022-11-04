c357598e77d7a662140f364dd6085ffeb0bf7781
Ezra Tampubolon <ezra.tampubolon@googlemail.com>
2022-11-04 23:13:33 +0100

    Create Class Multi index

--- exploratory_analysis/visualization.py
+++ exploratory_analysis/visualization.py
@@ -14,6 +14,14 @@ from exploratory_analysis import basic_functions as bf
 def boxplot_list(
     data: pd.DataFrame, x: Any, list_y: List[Any], list_axes=List[Axes]
 ) -> None:
+    """Plot boxplots from a list
+
+    Args:
+        data (pd.DataFrame): _description_
+        x (Any): _description_
+        list_y (List[Any]): _description_
+        list_axes (_type_, optional): _description_. Defaults to List[Axes].
+    """
     for y in list_y:
         sns.boxplot(
             ax=list_axes[list_y.index(y)],
@@ -27,6 +35,13 @@ def boxplot_list(
 def piechart_list(
     li_data: List[pd.Series], labels: List[str], list_axes: List[Axes]
 ) -> None:
+    """Plot piechart from a list
+
+    Args:
+        li_data (List[pd.Series]): _description_
+        labels (List[str]): _description_
+        list_axes (List[Axes]): _description_
+    """
 
     colors = sns.color_palette("pastel")[0:5]
 
@@ -82,49 +97,98 @@ def plot_boxplots(
     boxplot_list(data=data, x=x_feature, list_y=li_y_features, list_axes=axes)
 
 
+class MultiIdxDF:
+    """Class for handling multi index DF
+    """
+    def __init__(self, multi_idx_df: pd.DataFrame):
+        bf.raise_error(
+            (multi_idx_df.index.nlevels != 2),
+            ValueError,
+            "DF has to have 2 level indexes",
+        )
+        self.multi_idx_df = multi_idx_df
+
+    @property
+    def categories(self) -> List[str]:
+        """List of keys in the first level index
+        of DF
+        """
+        return bf.distinct(self.multi_idx_df.index.get_level_values(0))
+
+    @property
+    def sub_categories(self) -> List[str]:
+        """List of keys in the second level index
+        of DF
+        """
+        return bf.distinct(self.multi_idx_df.index.get_level_values(1))
+
+    def _separate_multi_idx_df(self, which_col: str) -> List[pd.Series]:
+        """Separate multi index DF in series respective to first level index
+
+        Args:
+            which_col (str): Column to extract for each series
+        """
+        return [
+            self.multi_idx_df.xs(category)[which_col]
+            for category in self.sub_categories
+        ]
+
+    def create_pie_chart(
+        self,
+        title: str,
+        col_plotted: str,
+        ordering_sub_category: Optional[list] = None,
+    ) -> None:
+        """Create pie charts from two-level-indexed DF. Each pie chart corresponds
+        to a first level index (a.k.a. category) and depicts the quantity specified
+        in 'col_plotted' input for each second level index.
+        Args:
+            data (pd.DataFrame): DF with multilevel index
+            title (str): Super title of the plot
+            col_plotted (str): Column name of the quantity to be visualize
+            ordering_sub_category (default=[]): List of second level indexes ordered
+                desirely. Optional!
+        """
+        bf.raise_error(
+            condition=(ordering_sub_category is not None)
+            and (Counter(ordering_sub_category) != Counter(self.sub_categories)),
+            error_class=ValueError,
+            msg="ordering_sub_category has to be a permutation of the second level indexes",
+        )
+
+        fig, axes = plt.subplots(1, len(self.categories), figsize=(25, 8))
+        fig.suptitle(title)
+
+        data_processed = [
+            bf.return_obj_val_in_list_order(data_raw, ordering_sub_category)
+            if ordering_sub_category
+            else data_raw
+            for data_raw in self._separate_multi_idx_df(which_col=col_plotted)
+        ]
+
+        labels = ordering_sub_category if ordering_sub_category else self.sub_categories
+
+        piechart_list(li_data=data_processed, labels=labels, list_axes=axes)
+
+
 def create_pie_chart_from_multi_idx_df(
-    data: pd.DataFrame,
+    multi_idx_df: pd.DataFrame,
     title: str,
     col_plotted: str,
     ordering_sub_category: Optional[list] = None,
 ) -> None:
-    """Create pie charts from two-level-indexed DF. Each pie chart corresponds
-    to a first level index (a.k.a. category) and depicts the quantity specified
-    in 'col_plotted' input for each second level index.
+    """Function for creating pie charts from multi index DF
+    respective to first level index
+
     Args:
-        data (pd.DataFrame): DF with multilevel index
-        title (str): Super title of the plot
-        col_plotted (str): Column name of the quantity to be visualize
-        ordering_sub_category (default=[]): List of second level indexes ordered
-            desirely. Optional!
+        multi_idx_df (pd.DataFrame): _description_
+        title (str): _description_
+        col_plotted (str): _description_
+        ordering_sub_category (Optional[list], optional): _description_. Defaults to None.
     """
 
-    bf.raise_error(
-        (data.index.nlevels != 2), ValueError, "DF has to have 2 level indexes"
+    MultiIdxDF(multi_idx_df=multi_idx_df).create_pie_chart(
+        title=title,
+        col_plotted=col_plotted,
+        ordering_sub_category=ordering_sub_category,
     )
-
-    categories = list(set(data.index.get_level_values(0)))
-    sub_categories = list(set(data.index.get_level_values(1)))
-
-    bf.raise_error(
-        condition=(ordering_sub_category is not None)
-        and (Counter(ordering_sub_category) != Counter(sub_categories)),
-        error_class=ValueError,
-        msg="ordering_sub_category has to be a permutation of the second level indexes",
-    )
-
-    fig, axes = plt.subplots(1, len(categories), figsize=(25, 8))
-    fig.suptitle(title)
-
-    li_data_raw = [data.xs(category)[col_plotted] for category in categories]
-
-    data_processed = [
-        bf.return_obj_val_in_list_order(data_raw, ordering_sub_category)
-        if ordering_sub_category
-        else data_raw
-        for data_raw in li_data_raw
-    ]
-
-    labels = ordering_sub_category if ordering_sub_category else sub_categories
-
-    piechart_list(li_data=data_processed, labels=labels, list_axes=axes)
